77570eae39159c0c9f213c8c18cbb999e45b3bfa
waki859 <glotply@gmail.com>
2018-07-12 17:46:26 +0900

    modified_2018_07_12

--- index.js
+++ index.js
@@ -9,137 +9,131 @@ const content = fs.readFileSync('client_secret.json');
 
 module.exports = (robot) => {
 
-  robot.respond(/start$/i, (res) => {
-    var howtouse = "";
-    howtouse += `このBOTは課題を伝えるためのものです。\n`;
-    howtouse += `下記のメッセージを送信することで、それに\n`;
-    howtouse += `対応したアクションを取ることができます。\n`;
-    howtouse += `"danger" -> 最も提出期限が近い課題表示\n`;
-    howtouse += `"must" -> 最も優先度の高い課題表示\n`;
-    howtouse += `"add" -> 課題追加\n`;
-    howtouse += `"clear" -> 達成した課題削除\n`;
-    howtouse += `"all" -> 課題一覧`;
-    res.send(howtouse);
+  robot.join((res) => {
+    res.send(`ne270235によるBOTです。startと送信してみてください！`);
   });
 
-
-  robot.respond(/danger$/i, (res) => {
-    var results = "";
-    authorize(JSON.parse(content), (auth) => {
-      const sheets = google.sheets({version: 'v4', auth});
-      sheets.spreadsheets.values.get({
-        spreadsheetId: '1_t7FLELQ6T08TYRI9F5cA3YFpbA0baVXOO8plMCr1jo',
-        range: 'A3:I',
-      }, (err, result) => {
-        if (err) {
-          return console.log('The API returned an error: ' + err);
-        }
-        const rows = result.data.values;
-        // 多次元配列([提出期限, 課題内容])
-        var dates = [];
-        // 課題が1つ以上あれば
-        if (rows.length > 0) {
-          rows.map((row) => {
-            // [0]年 [1]月 [2]日 [3]曜日 [4]時 [5]分 [6]年 [7]課題内容 [8]優先度
-            dates.push([new Date(row[0], row[1], row[2], row[4], row[5], 0), row[3], row[6], row[7], row[8]]);
-          });
-        } else {
-          console.log('No data found.');
-        }
-        // datesを昔順に並べ替える
-        dates.sort(
-          function(a, b){
-            return (a[0] < b[0] ? -1 : 1);
-          }
-        );
-        // 今現在の日時を取得
-        var nowdate = new Date();
-        // 課題が1つしかないとき
-        if ((rows.length == 1) && (nowdate < dates[0][0])){
-          results += `期限：${formatDate(dates[0][0], dates[0][1])}\n`;
-          results += `講義：${dates[0][2]}\n`;
-          results += `内容：${dates[0][3]}\n`;
-          results += `優先度：${dates[0][4]}`;;
-        }
-        // 課題が2つ以上あるとき
-        if (rows.length > 1) {
-          var flag = 0;
-          for (var i = 0 ; i < rows.length ; i++){
-            if((nowdate < dates[i][0]) && (flag == 0)){
-              results += `期限：${formatDate(dates[i][0], dates[i][1])}\n`;
-              results += `講義：${dates[i][2]}\n`;
-              results += `内容：${dates[i][3]}\n`;;
-              results += `優先度：${dates[i][4]}`;;
-              flag = 1;
-            }
-          }
-        }
-        res.send(results);
-      });
+  robot.respond(/start$/i, (res) => {
+    res.send({
+      question: "現在溜まっている課題を表示します。2つの観点から項目を選んでください。",
+      options: ["締切間近", "重要度"],
     });
   });
 
-  robot.respond(/must$/i, (res) => {
-    var results = "";
-    authorize(JSON.parse(content), (auth) => {
-      const sheets = google.sheets({version: 'v4', auth});
-      sheets.spreadsheets.values.get({
-        spreadsheetId: '1_t7FLELQ6T08TYRI9F5cA3YFpbA0baVXOO8plMCr1jo',
-        range: 'A3:I',
-      }, (err, result) => {
-        if (err) {
-          return console.log('The API returned an error: ' + err);
-        }
-        const rows = result.data.values;
-        // 多次元配列([提出期限, 課題内容])
-        var dates = [];
-        // 課題が1つ以上あれば
-        if (rows.length > 0) {
-          rows.map((row) => {
-            // [0]年 [1]月 [2]日 [3]曜日 [4]時 [5]分 [6]年 [7]課題内容 [8]優先度
-            dates.push([new Date(row[0], row[1], row[2], row[4], row[5], 0), row[3], row[6], row[7], row[8]]);
-          });
-        } else {
-          console.log('No data found.');
-        }
-        // datesを昔順に並べ替える
-        dates.sort(
-          function(a, b){
-            return (a[4] < b[4] ? -1 : 1);
-          }
-        );
-        // 今現在の日時を取得
-        var nowdate = new Date();
-        // 課題が1つしかないとき
-        if ((rows.length == 1) && (nowdate < dates[0][0])){
-          results += `期限：${formatDate(dates[0][0], dates[0][1])}\n`;
-          results += `講義：${dates[0][2]}\n`;
-          results += `内容：${dates[0][3]}\n`;
-          results += `優先度：${dates[0][4]}`;
-        }
-
-        // 課題が2つ以上あるとき
-        if (rows.length > 1) {
-          var flag = 0;
-          for (var i = 0 ; i < rows.length ; i++){
-            if ((nowdate < dates[i][0]) && (flag == 0)){
-              results += `期限：${formatDate(dates[i][0], dates[i][1])}\n`;
-              results += `講義：${dates[i][2]}\n`;
-              results += `内容：${dates[i][3]}\n`;
-              results += `優先度：${dates[i][4]}`;
-              flag = 1;
-              // 同じ優先度が2つ以上ある時の対応
-              if (dates[i][4] == dates[i+1][4]){
-                results += `\n`;
-                results += `-------------------------------------\n`;
-                flag = 0;
+  robot.respond("select", (res) => {
+    switch (res.json.options[res.json.response]) {
+      case "締切間近":
+        var results = "";
+        authorize(JSON.parse(content), (auth) => {
+          const sheets = google.sheets({version: 'v4', auth});
+          sheets.spreadsheets.values.get({
+            spreadsheetId: '1_t7FLELQ6T08TYRI9F5cA3YFpbA0baVXOO8plMCr1jo',
+            range: 'A3:I',
+          }, (err, result) => {
+            if (err) {
+              return console.log('The API returned an error: ' + err);
+            }
+            const rows = result.data.values;
+            // 多次元配列([提出期限, 課題内容])
+            var dates = [];
+            // 課題が1つ以上あれば
+            if (rows.length > 0) {
+              rows.map((row) => {
+                // [0]年 [1]月 [2]日 [3]曜日 [4]時 [5]分 [6]年 [7]課題内容 [8]優先度
+                dates.push([new Date(row[0], row[1], row[2], row[4], row[5], 0), row[3], row[6], row[7], row[8]]);
+              });
+            } else {
+              console.log('No data found.');
+            }
+            // datesを昔順に並べ替える
+            dates.sort(
+              function(a, b){
+                return (a[0] < b[0] ? -1 : 1);
+              }
+            );
+            // 今現在の日時を取得
+            var nowdate = new Date();
+            // 課題が1つしかないとき
+            if ((rows.length == 1) && (nowdate < dates[0][0])){
+              results += `期限：${formatDate(dates[0][0], dates[0][1])}\n`;
+              results += `講義：${dates[0][2]}\n`;
+              results += `内容：${dates[0][3]}\n`;
+              results += `優先度：${dates[0][4]}`;;
+            }
+            // 課題が2つ以上あるとき
+            if (rows.length > 1) {
+              var flag = 0;
+              for (var i = 0 ; i < rows.length ; i++){
+                if((nowdate < dates[i][0]) && (flag == 0)){
+                  results += `期限：${formatDate(dates[i][0], dates[i][1])}\n`;
+                  results += `講義：${dates[i][2]}\n`;
+                  results += `内容：${dates[i][3]}\n`;;
+                  results += `優先度：${dates[i][4]}`;;
+                  flag = 1;
+                }
               }
             }
-          }
-        }
-        res.send(results);
-      });
-    });
+            res.send(results);
+          });
+        });
+        break;
+
+      case "重要度":
+        var results = "";
+        authorize(JSON.parse(content), (auth) => {
+          const sheets = google.sheets({version: 'v4', auth});
+          sheets.spreadsheets.values.get({
+            spreadsheetId: '1_t7FLELQ6T08TYRI9F5cA3YFpbA0baVXOO8plMCr1jo',
+            range: 'A3:I',
+          }, (err, result) => {
+            if (err) {
+              return console.log('The API returned an error: ' + err);
+            }
+            const rows = result.data.values;
+            var dates = [];
+            if (rows.length > 0) {
+              rows.map((row) => {
+                dates.push([new Date(row[0], row[1], row[2], row[4], row[5], 0), row[3], row[6], row[7], row[8]]);
+              });
+            } else {
+              console.log('No data found.');
+            }
+            dates.sort(
+              function(a, b){
+                return (a[4] < b[4] ? -1 : 1);
+              }
+            );
+            var nowdate = new Date();
+            if ((rows.length == 1) && (nowdate < dates[0][0])){
+              results += `期限：${formatDate(dates[0][0], dates[0][1])}\n`;
+              results += `講義：${dates[0][2]}\n`;
+              results += `内容：${dates[0][3]}\n`;
+              results += `優先度：${dates[0][4]}`;
+            }
+
+            if (rows.length > 1) {
+              var flag = 0;
+              for (var i = 0 ; i < rows.length ; i++){
+                if ((nowdate < dates[i][0]) && (flag == 0)){
+                  results += `期限：${formatDate(dates[i][0], dates[i][1])}\n`;
+                  results += `講義：${dates[i][2]}\n`;
+                  results += `内容：${dates[i][3]}\n`;
+                  results += `優先度：${dates[i][4]}`;
+                  flag = 1;
+                  // 同じ優先度が2つ以上ある時の対応
+                  if (dates[i][4] == dates[i+1][4]){
+                    results += `\n`;
+                    results += `-------------------------------------\n`;
+                    flag = 0;
+                  }
+                }
+              }
+            }
+            res.send(results);
+          });
+        });
+        break;
+    }
   });
 };
 
